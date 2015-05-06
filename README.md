2d7d1b8357bd8714120aa04cb80f20ff0fbb9cb9
R33D3M33R <R33D3M33R@users.noreply.github.com>
2015-05-06 20:43:18 +0200

    Add bash script and update README.md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# linux-ut99-cache-manager
+# Unreal Tournament 99 Cache Manager for Linux
 A simple Linux Bash Script which serves as a Unreal Tournament 99 Cache Manager
+# Homepage
+http://andrej.mernik.eu/clanki/linux/utcachemanager/
---
+++ utcachemanager.sh
@@ -0,0 +1,43 @@
+#!bin/bash
+#Author: Andrej Mernik, 2008, http://andrej.mernik.eu/clanki/linux/utcachemanager/
+#License: GPLv3
+
+#Unreal Tournament install directory
+UTdir="/home/user/games/ut"
+#Unreal Tournament cache directory
+CacheDir="/home/user/.loki/ut/Cache"
+
+
+#keep reading from the cache.ini until the end
+while read inputline
+do
+#split the line to two pieces
+CODE=${inputline%=*}
+TMPREALNAME=${inputline#*=}
+#remove the carriage return
+REALNAME=`echo -e $TMPREALNAME | sed 's/\r//g' `
+
+#get the extension
+EXTENSION=${REALNAME#*.}
+
+if [ $EXTENSION ]
+then
+	#Unreal music file
+        if [ "$EXTENSION" == 'umx' ]; then mv "$CacheDir/$CODE.uxx" "$UTdir/Music/$REALNAME"
+	#unreal audio file
+        elif [ "$EXTENSION" == 'uax' ]; then mv "$CacheDir/$CODE.uxx" "$UTdir/Sounds/$REALNAME"
+	#Unreal system file 
+        elif [ "$EXTENSION" == 'int' ]; then mv "$CacheDir/$CODE.uxx" "$UTdir/System/$REALNAME"
+	#Unreal system file 
+        elif [ "$EXTENSION" == 'u' ]; then mv "$CacheDir/$CODE.uxx" "$UTdir/System/$REALNAME"
+	#Unreal texture file
+        elif [ "$EXTENSION" == 'utx' ]; then mv "$CacheDir/$CODE.uxx" "$UTdir/Textures/$REALNAME"
+        #unreal map file
+	elif [ "$EXTENSION" == 'unr' ]; then mv "$CacheDir/$CODE.uxx" "$UTdir/Maps/$REALNAME"
+	#else skip them
+        else echo "Skipping ..."
+	echo $inputline >> "tmpcache.ini"
+        fi
+fi
+done < $CacheDir/cache.ini
+mv "tmpcache.ini" "$CacheDir/cache.ini"
